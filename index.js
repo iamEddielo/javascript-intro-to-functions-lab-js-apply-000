9746a47f7769b2eeee1aab0a7ee737a319f465f4
Eddie Lopez <iameddielopez@gmail.com>
2019-02-14 22:01:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ function logWhisper(string) {console.log('hello')}
 function sayHiToGrandma(string){
   
   var uppercase = (string)
-  uppercase.toUpperCase(string) === uppercase // true
+  uppercase.toUpperCase() === uppercase // true
   if ((string) === uppercase)
   {
    return 'YES INDEED!'
    }
    
    var lowercase = (string)
-   lowercase.toLowerCase(string) === lowercase // true
+   lowercase.toLowerCase() === lowercase // true
    if ((string) === lowercase){
     return "I can\'t hear you!"}
    
