28a791b3dea9271d4f6006bd59f9e32d51163a74
Eddie Lopez <iameddielopez@gmail.com>
2019-02-14 22:25:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,8 +18,7 @@ function sayHiToGrandma(string){
     return 'I can\'t hear you!'
      
    }
-   
-   if ((string) === "I love you, Grandma.") {
+    if ((string) === "I love you, Grandma.") {
     return "I love you, too."
     }
 
